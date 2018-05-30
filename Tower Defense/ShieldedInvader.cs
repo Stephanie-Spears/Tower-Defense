256f00c2e1eb73c2c2ac4e70f7be5a7f3cfc1ed8
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-29 18:31:04 -0700

    add notes

--- Tower Defense/ShieldedInvader.cs
+++ Tower Defense/ShieldedInvader.cs
@@ -21,3 +21,8 @@
         }
     }
 }
+
+//Polymorphism -> objects behave differently dependent on the type of object that they are
+//-Subclasses of the parent class can provide their own implementations of parent methods. Adding virtual to the base class method allows for subclasses to override the virtual methods -> in the subclass use the keyword 'override' in place of 'virtual' defined in the parent
+//Virtual 
+// 
